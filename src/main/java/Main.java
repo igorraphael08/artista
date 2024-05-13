98213044289f7deeebbcaf8a0412021cdceb52bc
igorraphael08 <169068315+igorraphael08@users.noreply.github.com>
2024-05-13 14:58:08 +0000

    dia 13 de maio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,14 @@ public class Main {
                                           System.out.println("-------------------|");
     System.out.println(" banda: Limp Bizkit|  ");
     System.out.println("-------------------/");
-    System.out.println("-----------------")
-  
+    System.out.println("------álbum-----------");
+  System.out.println("álbum: Significant other-1999");
+  System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~")    ;    
+  System.out.println("Significant other é o segundo àlbum lançado pela banda limp bizkit, lançado em 22 de junho de 1999. O CD estreou em primeiro na billboard 200. Vendendo 834 000 em sua primeira semana de venda e foi nomeado a um Grammy Award naquele mesmo ano.");
+   System.out.println("---------------------------------");
+                                          System.out.println("o àlbum contém 16 músicas sendo:");
+                                          System.out.println("intro-37 segundos");
+                                          System.out.println("Just Like This")
   }
 
 
