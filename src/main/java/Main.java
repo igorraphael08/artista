82dee432d5ef33786199f61f46308c9ef8c58a69
igorraphael08 <169068315+igorraphael08@users.noreply.github.com>
2024-05-13 15:11:19 +0000

    13 de maio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,8 +9,23 @@ public class Main {
   System.out.println("Significant other é o segundo àlbum lançado pela banda limp bizkit, lançado em 22 de junho de 1999. O CD estreou em primeiro na billboard 200. Vendendo 834 000 em sua primeira semana de venda e foi nomeado a um Grammy Award naquele mesmo ano.");
    System.out.println("---------------------------------");
                                           System.out.println("o àlbum contém 16 músicas sendo:");
-                                          System.out.println("intro-37 segundos");
-                                          System.out.println("Just Like This")
+                                          System.out.println("intro-00:37");
+                                          System.out.println("Just Like This-3:35");
+                                            System.out.println("Nookie-4:49");
+                                          System.out.println("Break Stuff-2:46");
+                                          System.out.println("Re-Arranged-5:54");
+                                          System.out.println("I'm Broke-3:59");
+                                          System.out.println("Nobody Like You-4:20");
+                                          System.out.println("Don't Go Off Wandering-3:59");
+                                          System.out.println("9 Teen 90 Nine-4:36");
+                                          System.out.println("N 2 Gether Now-4:49");
+                                          System.out.println("Trust?-4:59");
+                                          System.out.println("No sex-3:54");
+                                          System.out.println("Show Me What You Got-4:26");
+                                          System.out.println("A Lesson Leared-2:40");
+                                          System.out.println("Outro-1:54");
+                                          System.out.println("Rant (Matt Pinfield)-1:42");
+                                          System.out.println("----------------------------------");
   }
 
 
